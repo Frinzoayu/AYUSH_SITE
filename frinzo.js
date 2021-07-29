595123a95eea9d7fbf22fe8de870b6d086c58444
Frinzoayu <72176709+Frinzoayu@users.noreply.github.com>
2021-07-29 09:01:21 +0530

    Add files via upload

---
+++ frinzo.js
@@ -0,0 +1,11 @@
+function toggle() {
+    let btn = document.getElementById("btn");
+    let navbar = document.getElementById("material-");
+    if(navbar.style.display!="block")
+    {
+        navbar.style.display ="block";
+       }
+       else{
+ navbar.style.display ="none";
+       }
+    }
